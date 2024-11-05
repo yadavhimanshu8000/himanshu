d619a702895a87b5f84d693ccdc6ef0509aa99e4
yadavhimanshu8000 <singhhimanshu8000@gmail.com>
2024-11-05 15:26:31 +0530

    assignement

--- PYTHON PROGRAMMING/ASSIGNMENTS/Module – 3 (Collections, functions and Modules)/programs/Q44.py
+++ PYTHON PROGRAMMING/ASSIGNMENTS/Module – 3 (Collections, functions and Modules)/programs/Q44.py
@@ -1,18 +1,27 @@
 # Write a Python program to read a random line from a file.
 
-
 import random
 
 file = open("himanshu.txt", 'r')
 
 lines = file.readlines()
 
-    # Check if the list is not empty
 if lines:
+    random_line= ramdom.choice(lines)
 
-    # Choose a random line from the list
-    random_line = random.choice(lines)
-    print("Random line : ", random_line)  # Print the line without trailing newline
 
-else:
-    print("The file is empty.")
+# import random
+
+#     # Open the file in read mode
+# file = open("Hearme.txt", 'r')
+
+#     # Read all lines into a list
+# lines = file.readlines()
+
+#     # Check if the list is not empty
+# if lines:
+#     # Choose a random line from the list
+#     random_line = random.choice(lines)
+#     print("Random line : ", random_line)  # Print the line without trailing newline
+# else:
+#     print("The file is empty.")
